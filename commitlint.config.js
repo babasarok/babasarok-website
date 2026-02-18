0553fa335299513f5e85afcaa4c107bf382062c8
Attila Greguss <floyd0122@gmail.com>
2026-02-18 20:06:37 +0000

    use type module

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  parserPreset: "conventional-changelog-conventionalcommits",
-  formatter: "@commitlint/format",
-  rules: {
-    "type-case": [2, "always", "pascal-case"],
-    "type-enum": [2, "always", ["Docs", "Feat", "Fix", "Refactor", "Test", "Dev", "Clean", "Content"]],
-  },
+export default {
+    extends: ["@commitlint/config-conventional"],
+    parserPreset: "conventional-changelog-conventionalcommits",
+    formatter: "@commitlint/format",
+    rules: {
+        "type-case": [2, "always", "pascal-case"],
+        "type-enum": [2, "always", ["Docs", "Feat", "Fix", "Refactor", "Test", "Dev", "Clean", "Content"]],
+    },
 };
