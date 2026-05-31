0b5be4cbcde168963ccfe91b0ba00fa5d5161033
Attila Greguss <floyd0122@gmail.com>
2026-05-31 23:25:40 +0100

    remove more stuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,2 @@
-// lazy sizes for image loading
-// TODO: use lazysizes in all pages
-import "lazysizes";
-
 import "./script";
 import "iconify-icon";
--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,5 +1,4 @@
 // TODO: Remove jquery and use vanilla javascript
 import $ from "jquery";
-import "magnific-popup/dist/jquery.magnific-popup";
 
 window.jQuery = window.$ = $;
