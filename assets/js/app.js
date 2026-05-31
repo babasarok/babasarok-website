7a91cf8246c97f8f63e334819682ae1578ca1368
Attila Greguss <floyd0122@gmail.com>
2026-05-31 23:19:30 +0100

    remove quicklink

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
-// app.js for concatenation of smaller libraryies
-// to reduce http requests of small files
-
-// Prefetch in-viewport links during idle time
-import { listen } from "quicklink/dist/quicklink.mjs";
-listen();
-
 // lazy sizes for image loading
 // TODO: use lazysizes in all pages
 import "lazysizes";
