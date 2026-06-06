df7f486bcce37ce2b1bf25a67a380c15d4e53e7f
Attila Greguss <floyd0122@gmail.com>
2026-06-06 10:53:13 +0100

    add test info box

--- src/components/OrderForm.svelte
+++ src/components/OrderForm.svelte
@@ -272,6 +272,17 @@
         products = [];
     }}>
     <h3 class="mb-4">Tervezzük meg a szettet!</h3>
+    <div class="flex items-start gap-2 mb-6 p-2 rounded bg-bg-secondary border border-yellow-200">
+        <Icon icon="mdi:info" class="shrink-0 text-2xl text-yellow-500" />
+        <p class="text-xs text-yellow-700">
+            Jelenleg a rendelési folyamat tesztelés alatt áll. Ha bármilyen problémát tapasztalsz, kérlek, jelezd nekünk
+            a <a
+                class="text-yellow-700 underline hover:text-yellow-900"
+                href={`mailto:${params.address.email}?subject=Babasarok rendelési probléma&body=Kérlek, írd le a problémát, ha lehet screenshot-tal együtt.&cc=attilagreguss@protonmail.com`}
+                >email címünkön</a
+            >.
+        </p>
+    </div>
     <div class="flex flex-col">
         <div class="flex flex-col gap-4 pb-6">
             <div class="flex items-center gap-2">
