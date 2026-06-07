bd8d631442672c8b59785de5e2fb7909e18e8497
Attila Greguss <floyd0122@gmail.com>
2026-06-07 23:09:01 +0100

    fix prod build

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,45 @@
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfigFn } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        svelte(),
-        tailwindcss({
-            optimize: true,
-        }),
-    ],
+const conf: UserConfigFn = () => {
+    const isWatch = process.argv.includes("--watch") || process.argv.includes("-w");
+    return {
+        plugins: [
+            svelte(),
+            tailwindcss({
+                optimize: true,
+            }),
+        ],
 
-    publicDir: false,
+        publicDir: false,
 
-    build: {
-        emptyOutDir: true,
-        watch: {
-            exclude: [
-                "hugo_stats.json",
-                "resources/**",
-                "assets/**",
-                "public/**",
-                "layouts/**",
-                "static/**",
-                "content/**",
-            ],
+        build: {
+            emptyOutDir: true,
+            watch: isWatch
+                ? {
+                      exclude: [
+                          "hugo_stats.json",
+                          "resources/**",
+                          "assets/**",
+                          "public/**",
+                          "layouts/**",
+                          "static/**",
+                          "content/**",
+                      ],
+                  }
+                : null,
+
+            outDir: "./assets/components",
+
+            lib: {
+                entry: "./src/elements.ts",
+                cssFileName: "elements",
+                formats: ["es"],
+                fileName: () => "elements.js",
+            },
         },
-        outDir: "./assets/components",
+    };
+};
 
-        lib: {
-            entry: "./src/elements.ts",
-            cssFileName: "elements",
-            formats: ["es"],
-            fileName: () => "elements.js",
-        },
-    },
-});
+export default defineConfig(conf);
