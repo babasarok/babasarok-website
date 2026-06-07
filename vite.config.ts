e0493e748224dd4809afd9ff795c94278e70787f
Attila Greguss <floyd0122@gmail.com>
2026-06-07 19:42:42 +0100

    make sure form is always up to date

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,31 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         svelte(),
         tailwindcss({
             optimize: true,
-        })
+        }),
     ],
 
     publicDir: false,
 
     build: {
-        outDir: "./static/components",
         emptyOutDir: true,
+        watch: {
+            exclude: [
+                "hugo_stats.json",
+                "resources/**",
+                "assets/**",
+                "public/**",
+                "layouts/**",
+                "static/**",
+                "content/**",
+            ],
+        },
+        outDir: "./assets/components",
 
         lib: {
             entry: "./src/elements.ts",
