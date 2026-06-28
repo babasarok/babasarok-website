47aaa2edcb70fdf1bbaf0234eb913a6effd52416
Attila Greguss <floyd0122@gmail.com>
2026-06-28 10:39:25 +0100

    remove package

--- tina/config.ts
+++ tina/config.ts
@@ -10,7 +10,6 @@ import { ProductCollection } from "./collections/product";
 import { MaterialCollection } from "./collections/material";
 import { DeliveryMethodCollection } from "./collections/delivery-method";
 import { ContactCollection } from "./collections/contact";
-// import { HtmlFieldPlugin, MarkdownFieldPlugin } from "react-tinacms-editor";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch =
