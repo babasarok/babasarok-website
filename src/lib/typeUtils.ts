f1f56bae83a167a1ff8fa11913d2180b21e455f5
Attila Greguss <floyd0122@gmail.com>
2026-07-01 09:56:51 +0100

    added type utils

--- src/lib/typeUtils.ts
+++ src/lib/typeUtils.ts
@@ -9,3 +9,17 @@ export type ExtractValues<T, U> = {
 export type Serialised<T> = {
   [K in keyof T]: T[K] extends { serialise: () => infer R } ? R : Serialised<T[K]>;
 };
+
+/** Returns `true` if T and U matches, return `false` otherwise */
+export type IfEquals<T, U, Y = true, N = false> =
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+  (<G>() => G extends T ? 1 : 2) extends <G>() => G extends U ? 1 : 2 ? Y : N;
+
+/**
+ * This is for build-time type-checking, will be stripped by tree-shaking because it does nothing
+ *
+ * @example
+ * AssertTrue<IfEquals<MyType, MyOtherType>>();
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-unnecessary-type-parameters
+export function AssertTrue<T extends true>(): void {}
